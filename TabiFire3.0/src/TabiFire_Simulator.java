2ba2e4282c4e6cefa87923ce416942e71396b489
jreamy <37675348+jreamy@users.noreply.github.com>
2018-03-26 20:49:02 -0700

    Update and rename TabiFire_Simulator.java to TabiFire3.0/src/TabiFire_Simulator.java

--- TabiFire3.0/src/TabiFire_Simulator.java
+++ TabiFire3.0/src/TabiFire_Simulator.java
@@ -2,7 +2,7 @@
 public class TabiFire_Simulator {
     public static void main(String args[])
     {
-        TabiFire tabi = new TabiFire();
-        tabi.setTitle("TabiFire 3.0");
+        TabiFire tabitha = new TabiFire();
+        tabitha.setTitle("TabiFire 3.0");
     }
 }
