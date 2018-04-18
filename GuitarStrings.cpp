22cf007543f4081e2d2f259baff07bc3d8bda304
jreamy <37675348+jreamy@users.noreply.github.com>
2018-04-18 00:04:23 -0700

    Add files via upload
    Frequency Getters

---
+++ GuitarStrings.cpp
@@ -0,0 +1,89 @@
+
+
+
+
+#include "Arduino.h"
+#include "GuitarStrings.h"
+
+
+/**
+ * Initialize the memory space
+ */
+GuitarStrings::GuitarStrings()
+{
+  // Initialize the arrays, set defaults
+  for (int i = 0; i < SAMPLES; i++)
+  {
+    _dataPoints[i] = 0;
+  }
+  
+  _pin = A0;
+  _startSample = millis();
+  _endSample = millis();
+}
+
+
+/**
+ * Set the pin number to sample
+ */
+void GuitarStrings::setSamplePin(int pinNumber)
+{
+  _pin = pinNumber;
+}
+
+/**
+ * Samples the pin 256 times, and sets the start and end times
+ */
+void GuitarStrings::sample()
+{
+  // Set the initial time of the sampling
+  _startSample = micros();
+
+  // Iterate through all the data
+  for (int i = 0; i < SAMPLES; i++)
+  {
+    // Set each data point to the pin value
+    _dataPoints[i] = analogRead(_pin);
+  }
+  
+  // Set the time the sampling ended
+  _endSample = micros();
+}
+
+/**
+ * Returns the time it took to get the data sample
+ */
+unsigned long GuitarStrings::getSampleTime()
+{
+  return _endSample - _startSample;
+}
+
+void GuitarStrings::printSample()
+{
+  Serial.println(-4);
+  Serial.println(_startSample);
+  Serial.println(_endSample);
+  for (int i = 0; i < SAMPLES; i++)
+  {
+    Serial.println(_dataPoints[i]);
+  }
+  Serial.println(-3);
+}
+
+void GuitarStrings::printPrettySample()
+{
+  Serial.println(-4);
+  Serial.print("[");
+  Serial.print(_startSample);
+  Serial.print(", ");
+  Serial.print(_endSample);
+  for (int i = 0; i < SAMPLES; i++)
+  {
+    Serial.print(", ");
+    Serial.print(_dataPoints[i]);
+  }
+  Serial.println("]");
+  Serial.println(-3);
+}
+
+
---
+++ GuitarStrings.h
@@ -0,0 +1,49 @@
+/* ================================================ *
+ *                                                  *
+ *               Guitar Stirng Library              *
+ *                                                  *
+ *      Designed to be a dynamic set of stirngs     *
+ *           Holds up to 6 buttons at once          *
+ *                                                  *
+ *      Calculates relative frequency of string     *
+ *         Calculates frequency of one string       *
+ *                                                  *
+ *              Written by: Jack Reamy              *
+ *                                                  *
+ * ================================================ */
+
+#ifndef GuitarStrings_h
+#define GuitarStrings_h
+
+#include "Arduino.h"
+
+#define SAMPLES 256
+
+class GuitarStrings
+{
+  private:
+    int _pin;
+    int _dataPoints[SAMPLES];
+    unsigned long _startSample;
+    unsigned long _endSample;
+  public:
+    GuitarStrings();
+    void setSamplePin(int pinNumber);
+    void sample();
+    unsigned long getSampleTime();
+    void printSample();
+    void printPrettySample();
+};
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
